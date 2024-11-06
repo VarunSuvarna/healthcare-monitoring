32421b3e21fa94824a2e449dc4b11f533bd8a0ac
VarunSuvarna <vs2152830@gmail.com>
2024-11-06 19:01:09 +0530

    Delete HealthDashboard.tsx

--- HealthDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import HealthDashboard from '@/components/HealthDashboard'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-24">
-      <HealthDashboard />
-    </main>
-  )
-}
