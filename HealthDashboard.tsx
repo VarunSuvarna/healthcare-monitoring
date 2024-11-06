2e37b94b7daa2e2f91cbe91114640f519903943f
VarunSuvarna <vs2152830@gmail.com>
2024-11-06 08:12:34 +0530

    Create HealthDashboard.tsx

---
+++ HealthDashboard.tsx
@@ -0,0 +1,9 @@
+import HealthDashboard from '@/components/HealthDashboard'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24">
+      <HealthDashboard />
+    </main>
+  )
+}
